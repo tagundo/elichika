6267322c07ee3faaeae4f153e6b40bce230c1206
RusdiNgawi <naufalfajrifix@gmail.com>
2024-09-10 22:23:11 +0700

    merge Add binary search for rewards

--- subsystem/user_story_member/insert_member_story.go
+++ subsystem/user_story_member/insert_member_story.go
@@ -3,13 +3,19 @@ package user_story_member
 import (
 	"elichika/client"
 	"elichika/userdata"
+	"elichika/utils"
 )
 
 func InsertMemberStory(session *userdata.Session, storyMemberMasterId int32) {
-	userStoryMember := client.UserStoryMember{
-		StoryMemberMasterId: storyMemberMasterId,
-		IsNew:               true,
-		AcquiredAt:          session.Time.Unix(),
+	userStoryMember := client.UserStoryMember{}
+	exist, err := session.Db.Table("u_story_member").
+		Where("user_id = ? AND story_member_master_id = ?", session.UserId, storyMemberMasterId).Get(&userStoryMember)
+	utils.CheckErr(err)
+	if !exist {
+		session.UserModel.UserStoryMemberById.Set(storyMemberMasterId, client.UserStoryMember{
+			StoryMemberMasterId: storyMemberMasterId,
+			IsNew:               true,
+			AcquiredAt:          session.Time.Unix(),
+		})
 	}
-	session.UserModel.UserStoryMemberById.Set(storyMemberMasterId, userStoryMember)
 }
