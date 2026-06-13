f8bb1d8946d6bd19219429dcf1d38af6560961f4
tagundo <tagundo@protonmail.ch>
2026-06-13 16:26:16 +0900

    Update shortcut.sh

--- bin/shortcut.sh
+++ bin/shortcut.sh
@@ -6,8 +6,8 @@ echo "cd $PWD && curl -L https://raw.githubusercontent.com/tagundo/elichika/refs
 chmod +x ~/run_elichika2 && \
 chmod +x ~/update_elichika2 && \
 chmod +x ~/menu_elichika2 && \
-chmod +x ~/fast_update_elichika2 && \
+chmod +x ~/basic_update_elichika2 && \
 echo "Use \"~/run_elichika2\" in termux to run the server!" && \
 echo "Use \"~/menu_elichika2\" in termux to run the menu!" && \
-echo "Use \"~/fast_update_elichika2\" in termux to update the server!"
-echo "Use \"~/update_elichika2\" in termux to update the server using basic logic (will be slower but will work even if you have a really old version)!"
+echo "Use \"~/update_elichika2\" in termux to update the server!"
+echo "Use \"~/_basic_update_elichika2\" in termux to update the server using basic logic (will be slower but will work even if you have a really old version)!"
