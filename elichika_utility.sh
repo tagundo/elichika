94ef6e537c6d26e21e9f4d1680f545715ab2516d
tagundo <292430723+tagundo@users.noreply.github.com>
2026-06-14 22:01:48 +0900

    Update elichika_utility.sh

--- elichika_utility.sh
+++ elichika_utility.sh
@@ -201,6 +201,7 @@ while true; do
                 clear
                 echo "==== Mod Menu ===="
 			echo "1. extract assetbundle from sukusta/packs or static or CDN"
+			echo "2. sifas_breast_tuner"
             echo "0. Back to Main Menu"
                 read -p "Enter your choice: " mod_option
 
@@ -210,6 +211,12 @@ while true; do
 						pkill elichika
 						python3 llas_asset_extractor.py
                         read -p "Press Enter to continue..." _dummy012
+                        ;;
+                    2)
+						clear
+						pkill elichika
+						python3 sifas_breast_tuner.py
+                        read -p "Press Enter to continue..." _dummy0123
                         ;;
                     0)
                         break
